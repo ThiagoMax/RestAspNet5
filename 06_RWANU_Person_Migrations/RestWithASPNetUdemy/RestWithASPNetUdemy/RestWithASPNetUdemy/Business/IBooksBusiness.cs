1b10fcd16b03edf57f2de0071c5ccb7f95838dd9
ThiagoMax <thiagomaximiliano21@gmail.com>
2022-05-25 16:40:43 -0300

    Final implementations of migrations

---
+++ 06_RWANU_Person_Migrations/RestWithASPNetUdemy/RestWithASPNetUdemy/RestWithASPNetUdemy/Business/IBooksBusiness.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNetUdemy.Model;
+using RWANU.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNetUdemy.Business
+{
+    public interface IBooksBusiness
+    {
+        Books Create(Books book);
+        Books FindById(long id);
+        List<Books> FindAll();
+        Books Update(Books book);
+        void Delete(long id);
+    }
+}
