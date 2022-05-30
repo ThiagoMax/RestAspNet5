5db1ccc113b96c4912bd57335e5298c955963944
ThiagoMax <thiagomaximiliano21@gmail.com>
2022-05-30 16:00:51 -0300

    Final implementation of generic repository

---
+++ 07_RWANU_Generic_Repository/RestWithASPNetUdemy/RestWithASPNetUdemy/RestWithASPNetUdemy/Repository/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNetUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNetUdemy.Repository
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+        bool Exists(long id);
+    }
+}
