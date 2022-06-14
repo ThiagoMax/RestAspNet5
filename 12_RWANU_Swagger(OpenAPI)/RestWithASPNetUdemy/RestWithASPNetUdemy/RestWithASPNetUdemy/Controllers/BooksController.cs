7cb04756b7507605306b12a3458cd30964a36c76
ThiagoMax <thiagomaximiliano21@gmail.com>
2022-06-14 16:55:43 -0300

    Adding customization to Books entity

--- 12_RWANU_Swagger(OpenAPI)/RestWithASPNetUdemy/RestWithASPNetUdemy/RestWithASPNetUdemy/Controllers/BooksController.cs
+++ 12_RWANU_Swagger(OpenAPI)/RestWithASPNetUdemy/RestWithASPNetUdemy/RestWithASPNetUdemy/Controllers/BooksController.cs
@@ -27,6 +27,10 @@ namespace RestWithASPNetUdemy.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType((200), Type = typeof(List<BooksVO>))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         [TypeFilter(typeof(HyperMediaFilter))]
         public IActionResult Get()
         {
@@ -34,6 +38,10 @@ namespace RestWithASPNetUdemy.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType((200), Type = typeof(BooksVO))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         [TypeFilter(typeof(HyperMediaFilter))]
         public IActionResult Get(long id)
         {
@@ -43,6 +51,9 @@ namespace RestWithASPNetUdemy.Controllers
         }
         
         [HttpPost]
+        [ProducesResponseType((200), Type = typeof(BooksVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         [TypeFilter(typeof(HyperMediaFilter))]
         public IActionResult Post([FromBody] BooksVO book)
         {
@@ -51,6 +62,9 @@ namespace RestWithASPNetUdemy.Controllers
         }
         
         [HttpPut]
+        [ProducesResponseType((200), Type = typeof(BooksVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         [TypeFilter(typeof(HyperMediaFilter))]
         public IActionResult Put([FromBody] BooksVO book)
         {
@@ -59,6 +73,9 @@ namespace RestWithASPNetUdemy.Controllers
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         public IActionResult Delete(long id)
         {
             _booksBusiness.Delete(id);
