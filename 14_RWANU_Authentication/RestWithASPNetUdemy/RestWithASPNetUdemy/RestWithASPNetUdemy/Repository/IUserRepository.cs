bd41c0b8e207bc1fea6c4196fcda5868c81f3070
ThiagoMax <thiagomaximiliano21@gmail.com>
2022-07-14 15:07:19 -0300

    Encrypt and validate methods for User

---
+++ 14_RWANU_Authentication/RestWithASPNetUdemy/RestWithASPNetUdemy/RestWithASPNetUdemy/Repository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RWANU.Data.VO;
+using RWANU.Model;
+
+namespace RWANU.Repository
+{
+    public interface IUserRepository
+    {
+        User ValidateCredentials(UserVO user);
+    }
+}
---
+++ 14_RWANU_Authentication/RestWithASPNetUdemy/RestWithASPNetUdemy/RestWithASPNetUdemy/Repository/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using RestWithASPNetUdemy.Model.Context;
+using RWANU.Data.VO;
+using RWANU.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RWANU.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _context;
+
+        public UserRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidateCredentials(UserVO user)
+        {
+            var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
+            return _context.Users.FirstOrDefault(u => (u.UserName == user.UserName) && (u.Password == pass));
+        }
+
+        private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
