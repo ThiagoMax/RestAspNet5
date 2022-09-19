a1f61d401863a8d65a34c88ed1b468a5f15a8596
ThiagoMax <thiagomaximiliano21@gmail.com>
2022-09-19 11:11:21 -0300

    QueryParams and PagedSearch

---
+++ 16_RWANU_QueryParams_PagedSearch/RestWithASPNetUdemy/RestWithASPNetUdemy/RestWithASPNetUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNetUdemy.Data.VO;
+using System.Collections.Generic;
+
+namespace RestWithASPNetUdemy.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonVO Create(PersonVO person);
+        PersonVO FindById(long id);
+        List<PersonVO> FindAll();
+        PersonVO Update(PersonVO person);
+        PersonVO Disable(long id);
+        void Delete(long id);
+    }
+}
---
+++ 16_RWANU_QueryParams_PagedSearch/RestWithASPNetUdemy/RestWithASPNetUdemy/RestWithASPNetUdemy/Repository/Generic/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNetUdemy.Model;
+using RWANU.Model.Base;
+using System.Collections.Generic;
+
+namespace RestWithASPNetUdemy.Repository
+{
+    public interface IPersonRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T FindById(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
